8c36b96673f2d8f9f60d33eb114db957454bf489
maximilianhp <sandra@studioantoine.com>
2021-07-21 13:51:08 +0200

    Projects Grid

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,7 +9,7 @@ const Projects = () => {
   const data = useStaticQuery(
     graphql`
       query {
-        allContentfulProject(sort: {fields: projectTitle, order: ASC}) {
+        allContentfulProject {
           edges {
             node {
               projectTitle
@@ -30,30 +30,89 @@ const Projects = () => {
   return (
     <Layout>
       <SEO title="Projects" />
-      <p>
-        <Link to="/">Go back to the homepage</Link>
-      </p>
-      <ul className="projects">
-        {data.allContentfulProject.edges.map(edge => {
-          return (
-            <li className="project" key={edge.node.id}>
-              <h2>
-                <Link to={`/projects/${edge.node.slug}/`}>{edge.node.projectTitle}</Link>
-              </h2>
-              {edge.node.featuredImage && (
-                <Img
-                  className="featured"
-                  fluid={edge.node.featuredImage.fluid}
-                  alt={edge.node.title}
-                />
-              )}
-              <div className="button">
-                <Link to={`/projects/${edge.node.slug}/`}>Read More</Link>
-              </div>
-            </li>
-          )
-        })}
-      </ul>
+      <div
+        style={{
+          position: `absolute`,
+          top: `80px`,
+          left: `0`,
+          minHeight: `calc(100vh - 80px)`,
+          width: `100vw`,
+          background: `#ffffff`,
+        }}
+      >
+        <div className="grid">
+          {data.allContentfulProject.edges.map(edge => {
+            return (
+              <Link
+                to={`/projects/${edge.node.slug}/`}
+                style={{
+                  position: `relative`,
+                  height: `0`,
+                  width: `0`,
+                  margin: `0`,
+                  padding: `0`,
+                }}
+              >
+                <div
+                  className="item"
+                  style={{
+                    position: `relative`,
+                    float: `left`,
+                    height: `25vw`,
+                    width: `25vw`,
+                    background: `#ffffff`,
+                    margin: `0`,
+                  }}
+                >
+                  {edge.node.featuredImage && (
+                    <Img
+                      className="featured"
+                      fluid={edge.node.featuredImage.fluid}
+                      alt={edge.node.title}
+                      style={{
+                        position: `absolute`,
+                        top: `0`,
+                        right: `0`,
+                        height: `25vw`,
+                        width: `25vw`,
+                        objectFit: `cover`,
+                      }}
+                    />
+                  )}
+                  <h2
+                    style={{
+                      position: `absolute`,
+                      left: `20px`,
+                      bottom: `40px`,
+                      fontSize: `14px`,
+                      fontFamily: `ogg`,
+                      color: `#212121`,
+                      margin: `0`,
+                      fontWeight: `normal`,
+                    }}
+                  >
+                    {edge.node.projectTitle}
+                  </h2>
+                  <h3
+                    style={{
+                      position: `absolute`,
+                      left: `20px`,
+                      bottom: `20px`,
+                      fontSize: `10px`,
+                      fontFamily: `relativebook`,
+                      color: `#212121`,
+                      margin: `0`,
+                      fontWeight: `normal`,
+                    }}
+                  >
+                    {edge.node.location}
+                  </h3>
+                </div>
+              </Link>
+            )
+          })}
+        </div>
+      </div>
     </Layout>
   )
 }
