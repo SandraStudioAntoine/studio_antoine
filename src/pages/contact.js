b59598573cc23bc9b9653c7381387c1bc953bade
maximilianhp <sandra@studioantoine.com>
2021-07-27 05:26:04 +0200

    Map api

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,8 +4,344 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import WordImg from "../images/wordmark.svg"
+import GoogleMapReact from 'google-map-react';
 
-const Contact = () => (
+const Contact = () => {
+  const mapOptions = {
+    styles: [
+      {
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape.natural",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#f5f5f2"
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "administrative",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.attraction",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape.man_made",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#ffffff"
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.business",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.medical",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.place_of_worship",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.school",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.sports_complex",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "color": "#ffffff"
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              },
+              {
+                  "color": "#ffffff"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "color": "#ffffff"
+              },
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "stylers": [
+              {
+                  "color": "#ffffff"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "stylers": [
+              {
+                  "color": "#ffffff"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.park",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "stylers": [
+              {
+                  "color": "#71c8d4"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "stylers": [
+              {
+                  "color": "#e5e8e7"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.park",
+          "stylers": [
+              {
+                  "color": "#8ba129"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "stylers": [
+              {
+                  "color": "#ffffff"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.sports_complex",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "color": "#c7c7c7"
+              },
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "stylers": [
+              {
+                  "color": "#a0d3d3"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.park",
+          "stylers": [
+              {
+                  "color": "#91b65d"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.park",
+          "stylers": [
+              {
+                  "gamma": 1.51
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "poi.government",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road"
+      },
+      {
+          "featureType": "road"
+      },
+      {},
+      {
+          "featureType": "road.highway"
+      },
+    ],
+    zoomControl: false,
+    fullscreenControl: false,
+  };
+
+  const Marker = () => {
+    return (
+      <div
+        className="marker-map"
+        style={{
+          height: `20px`,
+          width: `20px`,
+          borderRadius: `10px`,
+          backgroundColor: `#212121`,
+        }}
+      ></div>
+    );
+  };
+
+  return (
+
   <Layout>
     <Seo title="Contact" />
     <div
@@ -77,7 +413,9 @@ const Contact = () => (
           >hello@studioantoine.com</a>
         </p>
       </div>
-      <iframe
+
+      <div
+        className="mapGoogle"
         style={{
           border: `none`,
           width: `calc(50vw - 120px)`,
@@ -86,12 +424,22 @@ const Contact = () => (
           top: `160px`,
           right: `80px`,
         }}
-        loading="lazy"
-        allowfullscreen
-        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDi8Ijv1DcNQeOB9QoU-Lln_jot0CzdxJE&q=Space+Needle,Seattle+WA">
-      </iframe>
+      >
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: 'AIzaSyDi8Ijv1DcNQeOB9QoU-Lln_jot0CzdxJE',
+          }}
+          center={[19.41504720565826, -99.18189340261033]}
+          zoom={16}
+          options={mapOptions}
+          backgroundColor={'none'}
+        >
+          <Marker lat={19.41504720565826} lng={-99.18189340261033} text="My Marker"></Marker>
+        </GoogleMapReact>
+      </div>
     </div>
   </Layout>
-)
+  )
+}
 
 export default Contact
