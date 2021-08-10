12bfa76da06589a672d864eb1d8e2d03d24e9334
maximilianhp <sandra@studioantoine.com>
2021-08-10 20:23:50 +0100

    reset

--- src/templates/video.js
+++ src/templates/video.js
@@ -13,69 +13,41 @@ export const query = graphql`
           url
         }
       }
-
     }
   }
 `
 
 const Video = props => {
-  if (props.data.contentfulVideo.video.file) {
-    return (
-      <Layout>
-        <SEO title={props.data.contentfulVideo.projectTitle} />
-        <div
-          className="project-wrapper"
-          style={{
-            top: `80px`,
-            left: `0`,
-            height: `calc(100vh - 80px)`,
-            width: `100vw`,
-          }}
-        >
-          <div className="content">
-            
-              <video
-                className="archive-video"
-                src={props.data.contentfulVideo.video.file.url}
-                controls="true"
-                style={{
-                  top: `80px`,
-                  left: `0`,
-                  height: `calc(100vh - 80px)`,
-                  width: `100vw`,
-                  backgroundColor: `#212121`,
-                }}
-              >
-              </video>
-            
-          </div>
+  return (
+    <Layout>
+      <SEO title={props.data.contentfulVideo.projectTitle} />
+      <div
+        className="project-wrapper"
+        style={{
+          top: `80px`,
+          left: `0`,
+          height: `calc(100vh - 80px)`,
+          width: `100vw`,
+        }}
+      >
+        <div className="content">
+          <video
+            className="archive-video"
+            src={props.data.contentfulVideo.video.file.url}
+            controls="true"
+            style={{
+              top: `80px`,
+              left: `0`,
+              height: `calc(100vh - 80px)`,
+              width: `100vw`,
+              backgroundColor: `#212121`,
+            }}
+          >
+          </video>
         </div>
-      </Layout>
-    )
-  } else {
-    return (
-      <Layout>
-        <SEO title={props.data.contentfulVideo.projectTitle} />
-        <div
-          className="project-wrapper"
-          style={{
-            top: `80px`,
-            left: `0`,
-            height: `calc(100vh - 80px)`,
-            width: `100vw`,
-          }}
-        >
-          <div className="content">
-            
-              <p>Hello</p>
-            
-          </div>
-        </div>
-      </Layout>
-    )
-  }
-
-
+      </div>
+    </Layout>
+  )
 }
 
 export default Video
