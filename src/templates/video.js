dd353491773c373c63a892c036d72af17ee30def
maximilianhp <sandra@studioantoine.com>
2021-08-10 20:16:31 +0100

    File null

--- src/templates/video.js
+++ src/templates/video.js
@@ -19,7 +19,7 @@ export const query = graphql`
 `
 
 const Video = props => {
-  if (typeof props.data.contentfulVideo.video !== null) {
+  if (typeof props.data.contentfulVideo.video.file !== null) {
     return (
       <Layout>
         <SEO title={props.data.contentfulVideo.projectTitle} />
